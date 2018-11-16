6fe44e72884c67da604fbb60133181fcb05380d0
dang024 <you@example.com>
2018-11-16 13:13:55 -0500

    idk

--- login/login.php
+++ login/login.php
@@ -1,13 +1,10 @@
-
+<!-- HTML Login Form -->
 <!DOCTYPE html>
 <html>
-<!--  <head>
-    <title>Hello</title>
-  </head>  -->
       <body>
           <form method="POST" action="login.php">
-          Email: <input type="text" name="email">
-          Password: <input type="password" name="password">;
+          Email: <input type="text" name="email"><br/>
+          Password: <input type="password" name="password"><br/>
           <input type="submit" value="Login">
           </form>
       </body>
