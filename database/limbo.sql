58e6435451d6e31de603f2bc1da87e5da676ad8c
torispychalski <31544062+torispychalski@users.noreply.github.com>
2018-11-21 17:28:30 -0500

    removed the owner/finder tables so we can use the users tables

--- database/limbo.sql
+++ database/limbo.sql
@@ -34,60 +34,6 @@ VALUES ('Victoria', 'Spychalski', 'victoria.spychalski1@marist.edu'),
        ('Maria', 'Molloy', 'maria.molloy1@marist.edu');
 
 
-/*finder of an item is documented*/
-DROP TABLE IF EXISTS finder_t;
-CREATE TABLE finder_t
-(
-    FinderID INT PRIMARY KEY AUTO_INCREMENT,
-
-    FirstName TEXT NOT NULL,
-
-    LastName TEXT NOT NULL,
-
-    ItemFound TEXT NOT NULL,
-
-    DateReturned DATE NOT NULL,
-
-    FinderEmail TEXT NOT NULL
-);
-
-INSERT INTO finder_t (FirstName, LastName, ItemFound, DateReturned, FinderEmail)
-VALUES ('Hannah', 'Whitehead', 'Sunglasses', '2018-10-28' 'hannah.whitehead1@marist.edu'),
-
-       ('Alyssa', 'Lynch', 'Blue backpack', '2018-11-02','alyssa.lynch1@marist.edu'),
-
-       ('Dillon', 'Lusk', 'MacBook Pro', '2018-11-05' ,'maria.molloy1@marist.edu');
-
-
-/*someone who is looking for a lost item is an owner*/
-DROP TABLE IF EXISTS owner_t;
-CREATE TABLE owner_t
-(
-    OwnerID INT PRIMARY KEY AUTO_INCREMENT,
-
-    FirstName TEXT NOT NULL,
-
-    LastName TEXT NOT NULL,
-
-    ItemLost TEXT NOT NULL,
-
-    DateLost DATE NOT NULL,
-
-    BuildingLost TEXT NOT NULL,
-
-    OwnerEmail TEXT NOT NULL, /*when item is found, the owner will be emailed*/
-
-    ItemStatus TEXT NOT NULL /*itemstatus switches between 'not found' and 'found' when the item is returned*/
-
-);
-
-INSERT INTO owner_t (FirstName, LastName, ItemLost, DateLost, BuildingLost, OwnerEmail, ItemStatus)
-VALUES ('Lynn', 'Jacobs', 'Marist Water Bottle', '2018-10-21', 'Hancock', 'lynn.jacobs1@marist.edu', 'Not Found' ),
-
-       ('Steve', 'McGovern', 'Apple Watch', '2018-10-29', 'McCann', 'steve.mcgovern1@marist.edu', 'Not Found'),
-
-       ('Connor', 'Murray', 'Blue Backpack', '2018-11-01', 'Dyson', 'connor.murray1@marist.edu', 'Not Found');
-
 
 /*all found items will go here*/
 DROP TABLE IF EXISTS foundItems_t;
@@ -135,14 +81,3 @@ VALUES ('Marist WaterBottle', '2018-10-21', 'Hancock'),
 
        ('Blue Backpack', '2018-11-01', 'Dyson');
 
-
-SELECT * FROM presidents;
-
-
-SELECT lname, num, dob FROM presidents ORDER BY num;
-
-
-SELECT lname, num, dob FROM presidents ORDER BY lname;
-
-
-SELECT lname, num, dob FROM presidents ORDER BY dob DESC;
