8afc7b04e79b4111ac2c8ecc0def47cd4c974657
torispychalski <31544062+torispychalski@users.noreply.github.com>
2018-11-21 17:26:33 -0500

    changed primary key to auto-increment

--- database/users.sql
+++ database/users.sql
@@ -4,11 +4,10 @@ CREATE DATABASE IF NOT EXISTS site_db;
 USE site_db;
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id VARCHAR() NOT NULL,
+    user_id VARCHAR() PRIMARY KEY AUTO_INCREMENT,
     f_name  VARCHAR() NOT NULL,
     l_name  VARCHAR() NOT NULL,
     email   VARCHAR() NOT NULL,
     password  ,
-    PRIMARY KEY (user_id) ,
     UNIQUE (email)
 );
