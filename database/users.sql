5ca7c53deb17a6042d3ecb0be1fb7af485009174
torispychalski <31544062+torispychalski@users.noreply.github.com>
2018-11-21 17:30:22 -0500

    populated user table

--- database/users.sql
+++ database/users.sql
@@ -11,3 +11,11 @@ CREATE TABLE IF NOT EXISTS users (
     password  ,
     UNIQUE (email)
 );
+
+INSERT INTO users (f_name, l_name, email, password) 
+VALUES ('Hannah', 'Whitehead', 'hannah.whitehead1@marist.edu', 'password'),
+
+       ('Alyssa', 'Lynch','alyssa.lynch1@marist.edu', 'password'),
+
+       ('Dillon', 'Lusk', 'maria.molloy1@marist.edu', 'password');
+       
