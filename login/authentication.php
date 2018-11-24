0687f1de5718b4324b8baeb78cdb3ddc9fe511a3
dang024 <dangosimpson@gmail.com>
2018-11-24 12:57:05 -0500

    deleted authentication bc redundant

--- login/authentication.php
+++
@@ -1,7 +0,0 @@
-<!--Script that authenticates the correctness of login info-->
-<!DOCTYPE html>
-<html>
-  <body>
-<!-- i think this is the one that is supposed to include globals.php-->
-  </body>
-</html>
