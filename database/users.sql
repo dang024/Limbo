b7e44b68a7825a123056ec711f5f03245c57434a
torispychalski <31544062+torispychalski@users.noreply.github.com>
2018-11-24 23:17:07 -0500

    added admin priv

--- database/users.sql
+++ database/users.sql
@@ -9,10 +9,11 @@ CREATE TABLE IF NOT EXISTS users (
     l_name  VARCHAR(80) NOT NULL,
     email   VARCHAR(96) NOT NULL,
     password CHAR(32) NOT NULL,
+    AdminPriv VARCHAR(80) NOT NULL,
     UNIQUE (email)
 );
 
-INSERT INTO users (user_id, f_name, l_name, email, password)
-VALUES (0, 'Hannah', 'Whitehead', 'hannah.whitehead1@marist.edu', 'password'),
-       (1, 'Alyssa', 'Lynch','alyssa.lynch1@marist.edu', 'password'),
-       (2, 'Dillon', 'Lusk', 'maria.molloy1@marist.edu', 'password');
+INSERT INTO users (user_id, f_name, l_name, email, password, AdminPriv)
+VALUES ('Hannah', 'Whitehead', 'hannah.whitehead1@marist.edu', 'password', 1),
+       ('Alyssa', 'Lynch','alyssa.lynch1@marist.edu', 'password', 1),
+       ('Dillon', 'Lusk', 'maria.molloy1@marist.edu', 'password', 1);
