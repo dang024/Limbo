ab3edaa8154de6c41db7cc83390b41e083fc4ba2
torispychalski <31544062+torispychalski@users.noreply.github.com>
2018-11-27 11:22:57 -0500

    added a query

--- users/lost.php
+++ users/lost.php
@@ -1,4 +1,15 @@
-<!--Script that will be used to report a lost item-->
+<!--Script that will be used to report a found item-->
 <?php
 
+/* need to fix the query b/c the user cannot directly put things in the database*/
+
+    $LostItem = "INSERT INTO 'lostItems_t' (ItemName, DateLost, BuildingLost)
+                  VALUES('$ItemName', '$DateLost', '$BuildingLost')";
+
+    $result = mysqli_query($query,$con);
+      if($result){
+          echo 'You have reported your found item. The admin will approve your post within the next 24 hrs. Be sure to check back frequently to see if your item has been found!';
+      }
+      else
+          echo 'Oops somethings went wrong';
  ?>
