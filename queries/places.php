1e25d80f5b97a57c22183ff4f8e281b7456f4d09
torispychalski <31544062+torispychalski@users.noreply.github.com>
2018-11-26 19:55:04 -0500

    i think this is php syntax

--- queries/places.php
+++ queries/places.php
@@ -4,18 +4,18 @@
   <body>
     <?php
         function AddLostItem(
-            INSERT INTO lostitems_t (ItemName, DateLost, BuildingLost)
-            Values ( NameEntered, DateEntered, LocEntered);
+            INSERT INTO lostitems_t ($ItemName, $DateLost, $BuildingLost)
+            Values ( $NameEntered, $DateEntered, $LocEntered);
         )
         
         function AddFoundItem(
-            INSERT INTO founditems_t(ItemName, DateFound, BuildingFound)
-            VALUES(NameEntered, DateEntered, LocEntered)
+            INSERT INTO founditems_t($ItemName, $DateLost, $BuildingLost)
+            VALUES($NameEntered, $DateEntered, $LocEntered)
         )
         
         function LostToFound(
-            INSERT INTO foundItems(ItemName, DateFound, BuildingFound)
-            VALUES(LostName, LostDate, LostLoc)
+            INSERT INTO foundItems($ItemName, $DateLost, $BuildingLost)
+            VALUES($NameEntered, $DateEntered, $LocEntered)
         )
 ?>
       
