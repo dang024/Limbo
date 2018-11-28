ffcef096ec6914c5db11becd44991f268b9604d4
torispychalski <31544062+torispychalski@users.noreply.github.com>
2018-11-27 19:26:32 -0500

    could work

--- users/lost.php
+++ users/lost.php
@@ -1,6 +1,25 @@
 <!--Script that will be used to report a found item-->
 <?php
 
+ session_start();
+  //creates active connection to db
+  require 'C:\Users\danny\Desktop\Limbo\database\connect_db.php';
+    $itemName = $_POST["itemName"];
+    $dateLost = $_POST["dateLost"];
+    $buildingLost = $_POST["buildingLost"]
+    //echo "Welcome " . $f_name, ' ', $l_name;
+/* need to fix the query b/c the user cannot directly put things in the database*/
+
+    $LostItem = "INSERT INTO 'lostItems_t' (ItemName, DateLost, BuildingLost)
+                  VALUES('$ItemName', '$DateLost', '$BuildingLost')";
+
+    $result = mysqli_query($query,$con);
+      if($result){
+          echo 'You have reported your found item. The admin will approve your post within the next 24 hrs. Be sure to check back frequently to see if your item has been found!';
+      }
+      else
+          echo 'Oops somethings went wrong';
+
 /* need to fix the query b/c the user cannot directly put things in the database*/
 
   /*  $LostItem = "INSERT INTO 'lostItems_t' (ItemName, DateLost, BuildingLost)
