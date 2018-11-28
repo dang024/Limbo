0b4f28b3da470d4629cc8f92424f6313dc6de407
torispychalski <31544062+torispychalski@users.noreply.github.com>
2018-11-27 23:51:40 -0500

    changed query thing

--- users/lost.php
+++ users/lost.php
@@ -31,7 +31,7 @@
     $LostItem = "INSERT INTO 'lostItems_t' (ItemName, DateLost, BuildingLost)
                   VALUES('$ItemName', '$DateLost', '$BuildingLost')";
 
-    $result = mysqli_query($query,$con);
+    $result = mysqli_query($lostItem,$con);
       if($result){
           echo 'You have reported your found item. The admin will approve your post within the next 24 hrs. Be sure to check back frequently to see if your item has been found!';
       }
