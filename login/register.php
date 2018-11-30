19046246dd2b6c48ca97433affbb5a258298ebe6
mariamolloy <35545523+mariamolloy@users.noreply.github.com>
2018-11-30 00:25:25 -0500

    actually adds new user

--- login/register.php
+++ login/register.php
@@ -3,9 +3,25 @@
 <!--Confirm password for registering-->
 
 <!DOCTYPE html>
-<?php session_start(); ?>
+<html>
+
+<head>
+<title>Create an Account</title>
+ <link rel = "stylesheet" type = "text/css" href = "../homepage/limbostyle.css" />
+</head>
+
 <body>
-        <h2>Sign Up</h2>
+
+<ul>
+  <li><a href="../homepage/landing.php"> <img src="../homepage/limbobox.png" alt="Limbo Box" width="50" height="50"> </a></li>
+  <li><a href="../users/lost.php">Lost Items</a></li>
+  <li><a href="../users/found.php">Found Items</a></li>
+ <li><a href="../users/quicklinks.html">Quick Links</a></li>
+  <li><a href="../users/FAQ.html">FAQ</a></li>
+  <li style="float:right"><a href="../login/login.php">Login</a></li>
+        </ul>
+
+        <h2>Create an Account!</h2>
         <p>Please fill out the fields below to create an account.</p>
         <form method="POST" action="accountregister.php">
         First name: <input type="text" name="f_name">
@@ -17,8 +33,31 @@
         <br/>
         Confirm password: <input type="password" name="cpassword">
         <input type="submit" value="Register">
-        <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        <p>Already have an account? <a href="login.php">Login here</a></p>
         </form>
-    </div>
+
+<?php
+session_start();
+  //creates active connection to db
+  require '..\database\connect_db.php';
+    
+    
+    //echo "Welcome " . $f_name, ' ', $l_name;
+/* need to fix the query b/c the user cannot directly put things in the database*/
+  if (isset($_POST["f_name"]) & isset($_POST["l_name"]) & isset($_POST["email"]) & isset($_POST["password"])){
+    $f_name = $_POST["f_name"];
+    $l_name = $_POST["l_name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $LostItem = "INSERT INTO users_t (f_name, l_name, email, password)
+                  VALUES('$f_name', '$l_name', '$email', '$password')";
+                  $result = mysqli_query($con, $NewUser);
+                    if($result){
+                        echo 'Great job! You made an account!';
+                    }
+                    else
+                        echo 'Oops something\'s went wrong';
+}
+?>        
 </body>
 </html>
