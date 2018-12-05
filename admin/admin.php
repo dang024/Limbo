25f646e8ba169f8eb11e9dbff65cacc05973be74
dang024 <dangosimpson@gmail.com>
2018-12-05 16:04:58 -0500

    admin register

--- admin/admin.php
+++ admin/admin.php
@@ -137,23 +137,6 @@
       }
 
   mysqli_close( $con ) ;
-
-
-
-      /* probably needs like html or something to make this appear on the website*/
-      /* need to fix the query b/c the user cannot directly put things in the database*/
-
-   /*   $LostItem = "INSERT INTO 'lostItems_t' (ItemName, DateLost, BuildingLost)
-                    VALUES('$ItemName', '$DateLost', '$BuildingLost')";
-
-      $result = mysqli_query($query,$con);
-        if($result){
-            echo 'You have reported your found item. The admin will approve your post within the next 24 hrs. If you have not done so already, make sure you drop off the item at our office located in Hancock!';
-        }
-        else
-            echo 'Oops somethings went wrong';*/
-
-  //HTML form for reporting a found item
    ?>
    <!--Reporting Found items-->
    <h2>Report It</h2>
--- superadmin/superadmin.php
+++ superadmin/superadmin.php
@@ -1,9 +1,6 @@
-
-
 <!--Function that will be used to create a new user account-->
 <!--Has to check with users table-->
 <!--Confirm password for registering-->
-
 <!DOCTYPE html>
 <html>
 
@@ -18,14 +15,14 @@
   <li><a href="../homepage/landing.php"> <img src="../homepage/limbobox.png" alt="Limbo Box" width="50" height="50"> </a></li>
   <li><a href="../users/lost.php">Lost Items</a></li>
   <li><a href="../users/found.php">Found Items</a></li>
- <li><a href="../users/quicklinks.html">Quick Links</a></li>
+  <li><a href="../users/quicklinks.html">Quick Links</a></li>
   <li><a href="../users/FAQ.html">FAQ</a></li>
   <li style="float:right"><a href="../login/login.php">Login</a></li>
         </ul>
 
         <h2>Add Admin.</h2>
         <p>Please fill out the fields below to add an admin account.</p>
-        <form method="POST" action="register.php">
+        <form method="POST" action="accountregister.php">
         First name: <input type="text" name="f_name">
         Last name: <input type="text" name="l_name">
         Email: <input type="text" name="email">
@@ -58,7 +55,7 @@ session_start();
                         echo 'Great job! You made an account!';
                     }
                     else
-                        echo 'Oops something\'s went wrong';
+                        echo 'Oops something went wrong';
 }
 ?>
 </body>
