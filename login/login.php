e8dfa3e791db4fb5b30a48111777d19c95cb4b93
dang024 <you@example.com>
2018-12-07 02:54:50 -0500

    comments

--- login/login.php
+++ login/login.php
@@ -41,3 +41,37 @@
 
 
 </html>
+<!--function authorize($username, $password)
+{
+    // Notice the use of sha1 on the passwords, make sure you store your passwords with sha1 (avoid MD5)
+    return ($username == 'username' & sha1($password) == sha1('password')) ? 0 : -1;
+}
+// If the login form has been entered
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Check the user's credentials
+    $pid = authorize($_POST['username'], $_POST['password']);
+    if ($pid == -1) {
+        // If the user is not authorized, then show them a message saying they aren't allowed to be there
+        echo '<P style=color:red>Login failed please try again.</P>';
+    } else {
+        // If user _is_ authorized, set a session variable saying they are logged in, set the header, then load the page
+        session_start();
+        $_SESSION['logged_in'] = 'YES';
+        header("location: authorized_page.php");
+        load('authorized_page.php', $pid);
+    }
+}
+?>
+
+<h1>Demo login</h1>
+<form action="login.php" method="POST">
+    <table>
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" name="username"/></td>
+            <td>Password:</td>
+            <td><input type="password" name="password"/></td>
+        </tr>
+    </table>
+<p><input type="submit"></p>
+-->
